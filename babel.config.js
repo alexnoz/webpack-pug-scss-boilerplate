53bbd554e127b04544ec1ffc65a3a8f54084785a
Alex Nozdriukhin <alex-nozzz@mail.ru>
2018-07-19 21:46:29 +0300

    Babel esmodules target in development

--- babel.config.js
+++ babel.config.js
@@ -11,7 +11,7 @@ module.exports = api => {
         {
           modules: false,
           targets: {
-            esmodules: isProd
+            esmodules: !isProd
           }
         }
       ]
