1c5e95a6c74cf4e4cd85b375a34ae5bf7e774733
Khumar <khumarmusaeva@gmail.com>
2019-04-18 18:12:31 +0400

    some changes reverted

--- webpack.parts.js
+++ webpack.parts.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 
-const publicPath = ''
+const publicPath = '/'
 
 exports.publicPath = publicPath
 
